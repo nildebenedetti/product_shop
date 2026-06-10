8e37bfe97ed5326d04b0283af6d6ad39dd17b760
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-06-10 17:58:59 +0200

    Aggiunto controllo per assenza di reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,6 +2,13 @@ async function index(request, response) {
     try {
         const [rows] = await connection.query("SELECT * FROM reviews");
 
+        if (rows.length === 0) {
+            return response.status(404).json({
+                error: "Nessuna recensione trovata",
+                results: null
+            });
+        }
+
         response.json({
             error: null,
             results: rows
