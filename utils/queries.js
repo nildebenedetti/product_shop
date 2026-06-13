6bad30d5da9eb58389311953c507df50830a512d
“Nilde <nilde.benedetti@gmail.com>
2026-06-13 17:56:22 +0200

    spostate queries da categories  controller a utils/queries

--- utils/queries.js
+++ utils/queries.js
@@ -75,6 +75,30 @@ const queryDestroyReview = `delete r.*
 
 /* ====== CATEGORIES ===== */
 
+/* INDEX - Recupera l'intero catalogo organizzato per categorie, includendo tutti i dettagli dei prodotti.
+Usa il LEFT JOIN per mostrare TUTTE le categorie, comprese quelle che non hanno prodotti associati.
+Ideale per generare nel frontend un menu o un catalogo strutturato in macro-sezioni. */
+
+    const querySelectAllCategoriesandProducts = `    SELECT 
+    c.id as category_id, c.name AS category_name, c.category_type as category_type, c.short_description AS category_short_description, c.marketing_description as category_marketing_description, p.id AS product_id, p.name as product_name, p.short_description as product_short_description, p.marketing_description as product_marketing_description, p.ingredients, p.allergens, p.availability, p.price , p.image_url as imageUrl
+    FROM categories c
+    LEFT JOIN category_product cp ON c.id = cp.category_id
+    LEFT JOIN products p ON p.id = cp.product_id`;
+
+/* SHOW -  Recupera l'elenco dei soli prodotti appartenenti a una specifica categoria tramite category ID.
+Esegue una INNER JOIN escludendo le categorie vuote o i prodotti non categorizzati.
+Ideale per filtrare il catalogo quando l'utente clicca su una specifica sezione (es. "/categoria/5"). */
+
+        const querySelectProductByCateogryId = `
+    SELECT p.*
+    FROM products p
+    JOIN category_product pc 
+    ON p.id = pc.product_id
+    JOIN categories c ON pc.category_id = c.id
+    WHERE c.id = ? ;`;
+
+
+
 export {
     querySelectAll,
     querySelectById,
@@ -85,5 +109,7 @@ export {
     querySelectFeaturedReviews,
     queryShowAllProductReviews,
     queryCreateReview,
-    queryDestroyReview
+    queryDestroyReview,
+    querySelectAllCategoriesandProducts,
+    querySelectProductByCateogryId
 };
